ca3433bcb47ed8fbd218d10516342b790145ce03
Bohdan Tron <bohdan.tron@gmail.com>
2025-05-09 15:14:12 +0300

    feat: experiment with connecting dictionary api

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
-  def home():
-    return render_template('home.html')
+def home():
+  return render_template('home.html')
   
 @app.route('/api/v1/<station>/<date>')
-  def about(station, date):
+def api(station, date):
+  return {
+    "station": station,
+    "date": date,
+    "temperature": temperature,
+  }
+    
+@app.route('/api/v0/<word>')
+def word(word):
+  """
+    Extract word definition from https://api.dictionaryapi.dev
+  """
+  try:
+    r = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
+    if r.status_code == 200:
+      data = r.json()
+      
+      # Extract definition
+      definition = ''
+      if data and len(data) > 0:
+        for meaning in data[0].get("meanings", []):
+          for definition_item in meaning.get("definitions", []):
+            definition_text = definition_item.get("definition", "")
+            
+            definition = definition_text
+
+  except Exception as e:
     return {
-      "station": station,
-      "date": date,
-      "temperature": temperature,
+      "word": word,
+      "error": f"An error occurred: {str(e)}"
     }
+            
+  return {
+    "definition": definition,
+    "word": word,
+  }
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run(debug=True, port=5001)
